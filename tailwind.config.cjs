bb4a6d71bd0e38d5df17af732618ee073827280f
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2023-03-02 23:39:31 +0300

    resume update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -12,7 +14,8 @@ module.exports = {
           'ui-monospace',
           'Courier New',
           'monospace'
-        ]
+        ],
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans]
       }
     }
   },
